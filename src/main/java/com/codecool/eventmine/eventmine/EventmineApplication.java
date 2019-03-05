7b906b184774b1bc319437579028fcae6d101b85
Csilla Urban <csillaurban@gmail.com>
2019-03-05 15:07:12 +0100

    initial commit

---
+++ src/main/java/com/codecool/eventmine/eventmine/EventmineApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.eventmine.eventmine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventmineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventmineApplication.class, args);
+    }
+
+}
