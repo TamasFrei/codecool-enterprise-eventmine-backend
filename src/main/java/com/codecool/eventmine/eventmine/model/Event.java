e091b930d348684471b131dfcf23e4c933d9a9ca
kemenykristof <kemeny.kristof24@gmail.com>
2019-03-19 13:47:23 +0100

    Refactored event for db usage

--- src/main/java/com/codecool/eventmine/eventmine/model/Event.java
+++ src/main/java/com/codecool/eventmine/eventmine/model/Event.java
@@ -4,35 +4,20 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.Date;
+import java.time.LocalDate;
+import java.util.List;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public abstract class Event {
 
-    private static int counter = 1;
-    protected String eventId;
+
+    protected String id;
+
     protected String name;
     protected String location;
-    protected Date date;
-    protected int numOfRemainingTickets;
-    protected int price;
-
-
-    public Event(String name, String location, Date date, int numOfRemainingTickets, int price) {
-        this.eventId = name + counter;
-        counter++;
-        this.name = name;
-        this.location = location;
-        this.date = date;
-        this.numOfRemainingTickets = numOfRemainingTickets;
-        this.price = price;
-    }
-
-    public void removeTicket(int amount) {
-        this.numOfRemainingTickets -= amount;
-    }
-
+    protected LocalDate date;
+    protected List<Ticket> availableTickets;
 
 }
