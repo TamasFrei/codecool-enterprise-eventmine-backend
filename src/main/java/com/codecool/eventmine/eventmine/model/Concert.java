b56f30d07f3ca66a9f498aab4a43416588aa95cc
kemenykristof <kemeny.kristof24@gmail.com>
2019-03-19 13:48:23 +0100

    Refactored Concert for db usage

--- src/main/java/com/codecool/eventmine/eventmine/model/Concert.java
+++ src/main/java/com/codecool/eventmine/eventmine/model/Concert.java
@@ -13,9 +13,6 @@ public class Concert extends Event {
 
     private String performer;
 
-    public Concert(String name, String location, Date date, int numOfRemainingTickets, String performer, int price) {
-        super(name, location, date, numOfRemainingTickets, price);
-        this.performer = performer;
-    }
+
 
 }
