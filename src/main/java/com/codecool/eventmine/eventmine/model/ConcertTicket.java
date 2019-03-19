ffce6b987c220aabdc3ca9a24459acbde12563f8
kemenykristof <kemeny.kristof24@gmail.com>
2019-03-19 13:49:32 +0100

    Ticket refactored

--- src/main/java/com/codecool/eventmine/eventmine/model/ConcertTicket.java
+++ src/main/java/com/codecool/eventmine/eventmine/model/ConcertTicket.java
@@ -13,9 +13,5 @@ public class ConcertTicket extends Ticket {
 
     private PlaceType placeType;
 
-    public ConcertTicket(double price, Currency currency, String place, PlaceType placeType) {
-        super(price, currency, place);
-        this.placeType = placeType;
-    }
 
 }
--- src/main/java/com/codecool/eventmine/eventmine/model/Ticket.java
+++ src/main/java/com/codecool/eventmine/eventmine/model/Ticket.java
@@ -12,20 +12,9 @@ import java.util.Currency;
 @AllArgsConstructor
 public abstract class Ticket {
 
-    private int counter = 1;
     protected int id;
     protected double price;
     protected Currency currency;
     protected String place;
-
-
-
-    public Ticket(double price, Currency currency, String place) {
-        this.id = counter;
-        counter++;
-        this.price = price;
-        this.currency = currency;
-        this.place = place;
-    }
 
 }
