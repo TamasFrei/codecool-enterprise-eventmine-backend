61b0b082d419d925025bf04094a83b46b0326679
kemenykristof <kemeny.kristof24@gmail.com>
2019-03-19 10:17:26 +0100

    Concert refactored to use Lombok

--- src/main/java/com/codecool/eventmine/eventmine/model/ConcertTicket.java
+++ src/main/java/com/codecool/eventmine/eventmine/model/ConcertTicket.java
@@ -1,7 +1,14 @@
 package com.codecool.eventmine.eventmine.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.util.Currency;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ConcertTicket extends Ticket {
 
     private PlaceType placeType;
@@ -11,7 +18,4 @@ public class ConcertTicket extends Ticket {
         this.placeType = placeType;
     }
 
-    public PlaceType getPlaceType() {
-        return placeType;
-    }
 }
--- src/main/java/com/codecool/eventmine/eventmine/model/Event.java
+++ src/main/java/com/codecool/eventmine/eventmine/model/Event.java
@@ -34,4 +34,5 @@ public abstract class Event {
         this.numOfRemainingTickets -= amount;
     }
 
+
 }
