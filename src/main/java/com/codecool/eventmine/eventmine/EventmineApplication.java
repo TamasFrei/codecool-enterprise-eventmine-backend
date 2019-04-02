2ee109b104909120e47c22e85cd4ba2d9c28f63b
Tamas Frei <frei.tamas555@gmail.com>
2019-04-02 15:45:50 +0200

    Add app config and cross origin set in there

--- src/main/java/com/codecool/eventmine/eventmine/EventmineApplication.java
+++ src/main/java/com/codecool/eventmine/eventmine/EventmineApplication.java
@@ -1,24 +1,15 @@
 package com.codecool.eventmine.eventmine;
 
-import com.codecool.eventmine.eventmine.config.ControllerConfig;
 import com.codecool.eventmine.eventmine.service.EventCreator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class EventmineApplication {
 
     @Autowired
     private EventCreator eventCreator;
-
-    @Autowired
-    private ControllerConfig controllerConfig;
-
 
     public static void main(String[] args) {
         SpringApplication.run(EventmineApplication.class, args);
